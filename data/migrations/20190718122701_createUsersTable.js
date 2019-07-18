d8759f992c5e99d29671e4bbd1c8abc5dedee1ca
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-18 16:50:10 +0200

    added instruction table

--- data/migrations/20190718122701_createUsersTable.js
+++ data/migrations/20190718122701_createUsersTable.js
@@ -1,39 +1,49 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('recipes', table => {
     table.increments();
     table.text('recipe', 300).notNullable().unique()
-    table.text('instructions', 500).notNullable();
   })
-  .createTable('ingredients', table => {
-    table.increments();
-    table.text('ingredient', 300).notNullable();
-  })
-  .createTable("belonging", table => {
-    table.increments();
-    table
-      .integer("ingredient_id")
-      .unsigned()
-      .notNullable()
-      .references("id")
-      .inTable("ingredients")
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE')
+    .createTable('ingredients', table => {
+      table.increments();
+      table.text('ingredient', 300).notNullable();
+    })
+    .createTable("belonging", table => {
+      table.increments();
       table
-      .integer("recipe_id")
-      .unsigned()
-      .notNullable()
-      .references("id")
-      .inTable("recipes")
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
       table.text('quantity', 200).notNullable();
-  });
+    })
+    .createTable('instructions', table => {
+      table.increments();
+      table.text('instruction', 200);
+      table.integer('instruction_number').notNullable();
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes");
+    });
 };
 
-exports.down = function(knex) {
-  return knex.schema
-  .dropTableIfExists('recipes')
-  .dropTableIfExists('ingredients')
-  .dropTableIfExists('belonging');
-};
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('recipes')
+      .dropTableIfExists('ingredients')
+      .dropTableIfExists('belonging');
+  };
--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
   return knex('recipes').del()
     .then(function () {
       return knex('recipes').insert([
-        {id: 1, recipe: 'recipe1', instructions: 'sadfsaf'},
-        {id: 2, recipe: 'recipe2', instructions: 'trzt'},
-        {id: 3, recipe: 'recipe3', instructions: 'fdghfgh'}
+        {id: 1, recipe: 'recipe1'},
+        {id: 2, recipe: 'recipe2'},
+        {id: 3, recipe: 'recipe3'}
       ]);
     });
 };
